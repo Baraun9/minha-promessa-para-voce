043a47424d87ed477bfdb958e2561aa0bbf0ff90
Baraun9 <felipe.barauna.12@outlook.com>
2026-04-08 18:48:22 -0300

    Add files via upload

---
+++ src/script/song.js
@@ -0,0 +1,42 @@
+const music = document.getElementById('music');
+const btn = document.getElementById('btn');
+
+let click = 0;
+
+function song() {
+    if (click === 0 ) {
+        music.volume = 0.5;
+        music.play();
+        btn.textContent = 'Pois é...'
+        click = 1;
+    } else {
+        window.location.href = "src/pages/pg1.html";
+        click = 0;
+    }
+}
+
+function song2() {
+    if (click === 0 ) {
+        music.volume = 0.5;
+        music.currentTime = 52;
+        music.play();
+        btn.textContent = 'Próxima...'
+        click = 1;
+    } else {
+        window.location.href = "pg2.html";
+        click = 0;
+    }
+}
+
+function song3() {
+    if (click === 0 ) {
+        music.volume = 0.5;
+        music.currentTime = 153;
+        music.play();
+        btn.textContent = 'Início'
+        click = 1;
+    } else {
+        window.location.href = "../../../index.html";
+        click = 0;
+    }
+}
